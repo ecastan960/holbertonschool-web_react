7c8b20cca34776fd160fe3e7d6f68128fc9193c8
ecastan960 <ecastan1@eafit.edu.co>
2022-09-15 21:53:35 -0500

    [09] Task 2

--- 0x01-Webpack/task_2/js/dashboard_main.js
+++ 0x01-Webpack/task_2/js/dashboard_main.js
@@ -8,7 +8,7 @@ $.when($.ready).then(function () {
     counter++;
     $('#count').html(`${counter} clicks on the button`);
   };
-
+  $('body').append($('<div>').attr('id', 'logo'));
   $('body').append('<p>Holberton Dashboard</p>');
   $('body').append('<p>Dashboard data for the students</p>');
   $('body').append('<button>Click here to get started</button>');
